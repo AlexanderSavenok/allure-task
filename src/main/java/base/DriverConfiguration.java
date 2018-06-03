12f1a714cfc34d105380ba74eb4c24f6dd6bb4f6
Alexandor <alexandor.savenok@gmail.com>
2018-06-03 22:25:00 +0200

    Upload Patterns project with allure report stale

---
+++ src/main/java/base/DriverConfiguration.java
@@ -0,0 +1,32 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverConfiguration {
+
+    private WebDriver driver;
+
+    public WebDriver getWebDriver() {
+        if (driver == null) {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+            setDefaultSettings(driver);
+        }
+        return driver;
+    }
+
+    public void closeWebDriver() {
+        driver.close();
+        driver = null;
+    }
+
+    private void setDefaultSettings(WebDriver driver) {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
+    }
+}
